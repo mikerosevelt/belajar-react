07871b66564c2414eae6d866c505d072e2d72f15
mikerosevelt <zoe_abbas@aim.com>
2020-03-04 13:53:50 +0700

    learn post method

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -5,11 +5,17 @@ import axios from 'axios';
 
 class BlogPost extends Component {
     state = {
-        post: []
+        post: [],
+        formBlogPost: {
+            id: 1,
+            title: '',
+            body: '',
+            userId: 1
+        }
     }
 
     getPostAPI = () => {
-        axios.get('http://localhost:3004/posts')
+        axios.get('http://localhost:3004/posts?_sort=id&_order=desc')
             .then((res) => {
                 this.setState({
                     post: res.data
@@ -25,6 +31,30 @@ class BlogPost extends Component {
             })
     }
 
+    handleFormChange = (e) => {
+        let formBlogPostNew = { ...this.state.formBlogPost };
+        let timestamp = new Date().getTime();
+        formBlogPostNew['id'] = timestamp;
+        formBlogPostNew[e.target.name] = e.target.value;
+        this.setState({
+            formBlogPost: formBlogPostNew
+        })
+    }
+
+    postDataToAPI = () => {
+        axios.post(`http://localhost:3004/posts`, this.state.formBlogPost)
+            .then((res) => {
+                console.log(res);
+                this.getPostAPI();
+            }, (err) => {
+                console.log('error :', err);
+            })
+    }
+
+    handleSubmit = () => {
+        this.postDataToAPI();
+    }
+
     componentDidMount() {
         // GET API MENGGUNAKAN FETCH
         // fetch('https://jsonplaceholder.typicode.com/posts')
@@ -37,10 +67,18 @@ class BlogPost extends Component {
 
         this.getPostAPI()
     }
+
     render() {
         return (
             <Fragment>
                 <p className="section-title">Blog Post</p>
+                <div className="form-add-post">
+                    <label htmlFor="title">Title</label>
+                    <input type="text" name="title" placeholder="Add title" onChange={this.handleFormChange} />
+                    <label htmlFor="body">Blog Content</label>
+                    <textarea name="body" id="body" cols="30" rows="10" placeholder="Add blog content" onChange={this.handleFormChange}></textarea>
+                    <button className="btn-submit" onClick={this.handleSubmit}>Save</button>
+                </div>
                 {
                     this.state.post.map(post => {
                         return <Post key={post.id} data={post} remove={this.handleRemove} />
