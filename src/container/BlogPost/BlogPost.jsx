0875dd99a8a79d9ccdd739f06453884891f4a9b1
mikerosevelt <zoe_abbas@aim.com>
2020-03-04 18:54:55 +0700

    Learn method put

--- src/container/BlogPost/BlogPost.jsx
+++ src/container/BlogPost/BlogPost.jsx
@@ -11,7 +11,8 @@ class BlogPost extends Component {
             title: '',
             body: '',
             userId: 1
-        }
+        },
+        isUpdate: false
     }
 
     getPostAPI = () => {
@@ -34,7 +35,9 @@ class BlogPost extends Component {
     handleFormChange = (e) => {
         let formBlogPostNew = { ...this.state.formBlogPost };
         let timestamp = new Date().getTime();
-        formBlogPostNew['id'] = timestamp;
+        if (!this.state.isUpdate) {
+            formBlogPostNew['id'] = timestamp;
+        }
         formBlogPostNew[e.target.name] = e.target.value;
         this.setState({
             formBlogPost: formBlogPostNew
@@ -44,15 +47,51 @@ class BlogPost extends Component {
     postDataToAPI = () => {
         axios.post(`http://localhost:3004/posts`, this.state.formBlogPost)
             .then((res) => {
-                console.log(res);
+                // console.log(res);
                 this.getPostAPI();
+                this.setState({
+                    isUpdate: false,
+                    formBlogPost: {
+                        id: 1,
+                        title: '',
+                        body: '',
+                        userId: 1
+                    }
+                })
             }, (err) => {
                 console.log('error :', err);
             })
     }
 
+    putDataToAPI = () => {
+        axios.put(`http://localhost:3004/posts/${this.state.formBlogPost.id}`, this.state.formBlogPost)
+            .then((res) => {
+                this.getPostAPI();
+                this.setState({
+                    isUpdate: false,
+                    formBlogPost: {
+                        id: 1,
+                        title: '',
+                        body: '',
+                        userId: 1
+                    }
+                })
+            })
+    }
+
     handleSubmit = () => {
-        this.postDataToAPI();
+        if (this.state.isUpdate) {
+            this.putDataToAPI();
+        } else {
+            this.postDataToAPI();
+        }
+    }
+
+    handleUpdate = (data) => {
+        this.setState({
+            formBlogPost: data,
+            isUpdate: true
+        })
     }
 
     componentDidMount() {
@@ -74,14 +113,14 @@ class BlogPost extends Component {
                 <p className="section-title">Blog Post</p>
                 <div className="form-add-post">
                     <label htmlFor="title">Title</label>
-                    <input type="text" name="title" placeholder="Add title" onChange={this.handleFormChange} />
+                    <input type="text" value={this.state.formBlogPost.title} name="title" placeholder="Add title" onChange={this.handleFormChange} />
                     <label htmlFor="body">Blog Content</label>
-                    <textarea name="body" id="body" cols="30" rows="10" placeholder="Add blog content" onChange={this.handleFormChange}></textarea>
+                    <textarea value={this.state.formBlogPost.body} name="body" id="body" cols="30" rows="10" placeholder="Add blog content" onChange={this.handleFormChange}></textarea>
                     <button className="btn-submit" onClick={this.handleSubmit}>Save</button>
                 </div>
                 {
                     this.state.post.map(post => {
-                        return <Post key={post.id} data={post} remove={this.handleRemove} />
+                        return <Post key={post.id} data={post} remove={this.handleRemove} update={this.handleUpdate} />
                     })
                 }
             </Fragment>
